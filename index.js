1d26dee1328ba6d5f490671fdf8d3794a50db50f
Your Name <you@example.com>
2024-02-20 15:13:12 +1100

    [ADD] readme file

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 import { Configuration, OpenAIApi } from 'openai';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 dotenv.config() // to use dotenv
+
+const configuration = new Configuration({ apiKey: process.env.OPENAI_API_KEY})
+const openai = new OpenAIApi(configuration);
+
+async function main() {
+    const chatCompletion = await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages:[
+            {role: 'user', content: 'What is the best team in the NBA?'},
+
+        ],
+    });
+}
+
+main()
